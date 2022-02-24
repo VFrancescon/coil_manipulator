de8510d57b5b05b0a0a3edd4d813d79d2d98dab9
Vittorio <vittorio.francescon@gmaill.com>
2022-02-24 11:19:08 +0000

    Implemented Hex conversion functions.
    Hex to Dec (Unconverted)
    Hex to Value (Float w/ conversion factor)
    Dec to Hex (straight into input vector)
    
    Also added argc/argv stuff, will be needed later

--- include/PSULib_test.hpp
+++ include/PSULib_test.hpp
@@ -1,13 +1,39 @@
 #include <DxkdpLib/DxkdpLib.hpp>
 #include <DxkdpLib/instructions.h>
 
-#include <CppLinuxSerial/SerialPort.hpp>
-#include <CppLinuxSerial/Exception.hpp>
-
+#include <cstdlib>
 #include <cmath>
 #include <stdint.h>
 
-void SysInfoDecoder(output_message &out);
+void Decoder2B(output_message &msgOut);
 
-int main(void);
+/*Conversion functions*/
+
+/*
+Hex to Int, no conversion
+MSB = MSB read
+LSB = LSB read
+*/
+int HexToDec(uint8_t MSB, uint8_t LSB);
+
+/*Hex to Float, with conversion factor 
+MSB = MSB read
+LSB = LSB read
+Conv = Conversion factor (10^-value_read)
+*/
+float HexToValue(uint8_t MSB, uint8_t LSB, float Conv);
+
+/*Float to 16bit Hex, writes Low and High bit in the input_msg struct direclty.
+value = value to convert
+Conv = conversion factor
+entry = 1 writes to CONT1, CONT2
+entry = 2 writes to CONT3, CONT4 */
+void DecToHex(float value, float Conv, input_message &msgIn, int entry = 1);
+
+/*Conversion values
+AKA 10^-value_read
+Multiply ints by this to get the floats of actual values*/
+float V_conv, I_conv;
+
+int main(int argc, char *argv[]);
 
--- src/PSULib_test.cpp
+++ src/PSULib_test.cpp
@@ -1,78 +1,121 @@
 #include <PSULib_test.hpp>
 
-float V_conv, I_conv;
 
-void SysInfoDecoder(output_message &out){
-    std::cout << "\nSize of given vect is:" << out.output3.size();
-    // for(int i = 0; i < 20; i++) printf("at %d contains %02X\n", i, out.output[i]);
-    //for(auto i : out.output1) printf("%02X ", i );
+/*
+Takes Voltage, Current and Step Size from the PSUs.
+A bit unsure what the voltage actually represents as of now.
+*/
+void Decoder2B(output_message &msgOut){
     
-    out.ADDR = out.output3[1];
-    out.CODE = out.output3[2];
-    out.LENGTH = out.output3[3];
-    out.V_STEP = out.output3[4];
-    out.I_STEP = out.output3[5];
-
-    V_conv = pow(10, -int(out.V_STEP));
-    I_conv = pow(10, -int(out.I_STEP));
-    printf("\nVSTEP %02X hex, ISTEP %02X hex", out.V_STEP, out.I_STEP);
-    printf("\nV_Conv: %f  I_Conv: %f ", V_conv, I_conv);
-    
-
-    out.V_HIGH = out.output3[9];
-    out.V_LOW = out.output3[10];
-
-    printf("\n\nVH %02X VL %02X", out.V_HIGH, out.V_LOW);
-    uint16_t V_H = (out.V_HIGH << 8) + (out.V_LOW);
-    printf("\nVoltage: %04X Hex %d Dec ",V_H, V_H);
-    printf("Therefore V= %f", float(V_H)*V_conv);
-
-    out.I_HIGH = out.output3[11];
-    out.I_LOW = out.output3[12];
-
-    printf("\n\nIH %02X IL %02X", out.I_HIGH, out.I_LOW);
-    uint16_t I_H = (out.I_HIGH << 8) + (out.I_LOW);
-    printf("\nCurrent: %04X Hex %d Dec ",I_H, I_H);
-    printf("Therefore I= %f", float(I_H)*I_conv);
-
-}
-
-int main(void){
-    float V_conv, I_conv;
-
-
-
-    std::cout << "Press enter to begin";
-    std::cin.get();
-    std::string str = "/dev/ttyUSB0";
-    DXKDP_PSU PSU(str);
-    
-    input_message msgIn;
-    std::cout << "\nInput command: ";
-    for(auto i:msgIn.instruction) printf("%02X ", i);
-    PSU.PsuWrite(msgIn);
-
-    output_message msgOut;
-    PSU.PsuRead(msgOut);
+    /*Block here is for debugging of the two vectors read*/
+    //std::cout << "\nSize of given vect is:" << msgOut.output3.size();
     // std::cout << "\nsize of first output: " << msgOut.output1.size();
     // std::cout << "\nContents of first output: ";
     // for(auto i : msgOut.output1) printf("%02X ", i);
-    
     // std::cout << "\nsize of second output: " << msgOut.output2.size();
     // std::cout << "\nContents of second output: ";
     // for(auto i : msgOut.output2) printf("%02X ", i);
     
-    msgOut.output3.insert(msgOut.output3.end(), msgOut.output1.begin(), msgOut.output1.end());
-    msgOut.output3.insert(msgOut.output3.end(), msgOut.output2.begin(), msgOut.output2.end());
-
+    /*Next 2 lines combine the vectors into something usable*/
+    // msgOut.output3.insert(msgOut.output3.end(), msgOut.output1.begin(), msgOut.output1.end());
+    // msgOut.output3.insert(msgOut.output3.end(), msgOut.output2.begin(), msgOut.output2.end());
     std::cout << "\n----------------\n Concatenation\n\n";
     std::cout << "\nSize of concatenated vector: " << msgOut.output3.size();
     std::cout << "\nContents: ";
     for(auto i : msgOut.output3) printf("%02X ", i);
 
-    SysInfoDecoder(msgOut);
+
+
+    msgOut.ADDR = msgOut.output3[1];
+    msgOut.CODE = msgOut.output3[2];
+    msgOut.LENGTH = msgOut.output3[3];
+    msgOut.V_STEP = msgOut.output3[4];
+    msgOut.I_STEP = msgOut.output3[5];
+
+    V_conv = pow(10, -int(msgOut.V_STEP));
+    I_conv = pow(10, -int(msgOut.I_STEP));
+    printf("\nVSTEP %02X hex, ISTEP %02X hex", msgOut.V_STEP, msgOut.I_STEP);
+    printf("\nV_Conv: %f  I_Conv: %f ", V_conv, I_conv);
+
+    msgOut.V_HIGH = msgOut.output3[9];
+    msgOut.V_LOW = msgOut.output3[10];
+
+    msgOut.I_HIGH = msgOut.output3[11];
+    msgOut.I_LOW = msgOut.output3[12];
+
+    // float VoltageFloat = (float)HexToDec(msgOut.V_HIGH, msgOut.V_LOW) * V_conv;
+    // std::cout << "\nCalculated in func, V float is: " << VoltageFloat;
+    // float CurrentFloat = (float)HexToDec(msgOut.I_HIGH, msgOut.I_LOW) * I_conv;
+    // std::cout << "\nCalculated in func, I float is: " << CurrentFloat;
+
+    float VoltageFloat = HexToValue(msgOut.V_HIGH, msgOut.V_LOW, V_conv);
+    std::cout << "\nCalculated in func, V float is: " << VoltageFloat;
+    float CurrentFloat = HexToValue(msgOut.I_HIGH, msgOut.I_LOW, I_conv);
+    std::cout << "\nCalculated in func, I float is: " << CurrentFloat;
+
+}
+
+/*Takes high and low bit, combines them into an int*/
+int HexToDec(uint8_t MSB, uint8_t LSB){
+    uint16_t combinedHex = (MSB << 8) + LSB;
+    return (int)combinedHex;
+}
+
+float HexToValue(uint8_t MSB, uint8_t LSB, float Conv){
+    uint16_t combinedHex = (MSB << 8) + LSB;
+    return (float)combinedHex * Conv;
+}
+
+void DecToHex(float value, float Conv, input_message &msgIn, int entry){
+    uint16_t hexConverted = int(value/Conv);
+    switch (entry)
+    {
+    case 2:
+        msgIn.CONT3 = (uint8_t)hexConverted;
+        msgIn.CONT4 = (uint8_t)(hexConverted >> 8);
+        break;
+    
+    default:
+        msgIn.CONT1 = (uint8_t)hexConverted;
+        msgIn.CONT2 = (uint8_t)(hexConverted >> 8);
+        break;
+    }
+
+    // printf("lsb %02X ", msgIn.CONT1);
+    // printf("msb %02X ", msgIn.CONT2);
+
+}
+
+int main(int argc, char *argv[]){
+    std::cout << "Press enter to begin";
+    std::cin.get();
+    float cmd_line;
+    input_message msgIn;
+    switch(argc)
+    {
+    case 2:
+            cmd_line = atof(argv[1]);
+            DecToHex(cmd_line, 0.01, msgIn);
+        break;
+    default:
+            
+            std::string str = "/dev/ttyUSB0";
+            DXKDP_PSU PSU(str);
+            
+            
+            std::cout << "\nInput command: ";
+            for(auto i:msgIn.instruction) printf("%02X ", i);
+            //PSU.PsuWrite(msgIn);
+
+            output_message msgOut;
+            //PSU.PsuRead(msgOut);
+            Decoder2B(msgOut);
+        break;
+    }
+    
+
+
 
     std::cout << "\nEnd of program.\n";
-    //std::cin.get();
 
 }
