f413c38ede649b4d278fa7055281930ea7b99ce0
Vittorio Francescon <vittorio.francescon@gmail.com>
2022-03-07 15:53:29 +0000

    Lakeshore F71 Teslameter - Prototype Library
    Wrote a prototype of the F71 Teslameter Library.
    
    Requires testing.

---
+++ libs/include/LakeshoreF71Lib/LakeshoreF71Lib.hpp
@@ -0,0 +1,24 @@
+#include <CppLinuxSerial/SerialPort.hpp>
+#include <string.h>
+using namespace mn::CppLinuxSerial;
+
+class Teslameter{
+private:
+    void TeslameterSetup();
+    ~Teslameter();
+
+    std::string autoRange = "SENS:FIEL:RANG:AUTO 1";
+    std::string fieldTesla = "UNIT:FIEL TESL";
+    std::string fetchField = "FETC:FIEL:DC?";
+
+    std::string confirmed = "*OPC";
+
+public:
+    SerialPort serialPort;
+
+    Teslameter();
+    Teslameter(std::string COM);
+
+    float SingleAxisReading(int AXIS);
+    std::vector<float> AllAxisReading();
+};
