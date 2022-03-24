41ac737b4a5e4f1d40b91bd5c5d5d6bcbe279f96
Vittorio Francescon <vittorio.francescon@gmail.com>
2022-03-24 16:15:25 +0000

    Wrote outline for HCoilMiddlewareLib
    Started an outline.
    
    Needs to be tested, but should in practice work just fine.

---
+++ libs/include/HCoilMiddlewareLib/HCoilMiddlewareLib.hpp
@@ -0,0 +1,45 @@
+#include <DxkdpLib/DxkdpLib.hpp>
+#include <LakeshoreF71Lib/LakeshoreF71Lib.hpp>
+#include <LinActuatorLib/LinActuatorLib.hpp>
+#include <thread>
+
+
+
+class MiddlewareLayer{
+
+private:
+
+public:
+    /*
+    3x PSUs
+    1x Teslameter
+    1x Linear Actuator
+    */
+    // std::shared_ptr<Device> device;
+    std::unique_ptr<DXKDP_PSU> PSU_X;
+    std::unique_ptr<DXKDP_PSU> PSU_Y;
+    std::unique_ptr<DXKDP_PSU> PSU_Z;
+
+    std::unique_ptr<Teslameter> T_Meter;
+    std::unique_ptr<LinearActuator> LinAct; 
+
+    /*
+    Default constructor.
+    Assumes the following COM Ports:
+    0 -> PSUX
+    1 -> PSUY
+    2 -> PSUZ
+    3 -> Teslameter
+    4 -> Introducer
+    */
+    MiddlewareLayer();
+
+    MiddlewareLayer(std::string PSUX_PORT, std::string PSUY_PORT, std::string PSUZ_PORT, std::string TMETER_PORT, std::string LINACT_PORT );
+    // ~MiddlewareLayer();
+
+    void TurnOnSupply();
+    void TurnOffSupply();
+    void initialSetup();
+    void set3DVector(std::vector<float> I_X, std::vector<float> I_Y, std::vector<float> I_Z);
+
+};
---
+++ libs/src/HCoilMiddlewareLib.cpp
@@ -0,0 +1,65 @@
+#include <HCoilMiddlewareLib/HCoilMiddlewareLib.hpp>
+
+MiddlewareLayer::MiddlewareLayer(){
+    /*
+    3x PSUs
+    1x Teslameter
+    1x Linear Actuator
+    */
+    std::unique_ptr<DXKDP_PSU> PSU_X = std::make_unique<DXKDP_PSU>("/dev/ttyUSB0", 0.01, 0.01);
+    std::unique_ptr<DXKDP_PSU> PSU_Y = std::make_unique<DXKDP_PSU>("/dev/ttyUSB1", 0.01, 0.01);
+    std::unique_ptr<DXKDP_PSU> PSU_Z = std::make_unique<DXKDP_PSU>("/dev/ttyUSB2", 0.1, 0.1);
+
+    std::unique_ptr<Teslameter> T_Meter = std::make_unique<Teslameter>("/dev/ttyUSB3");
+    std::unique_ptr<LinearActuator> LinAct = std::make_unique<LinearActuator>("/dev/ttyUSB4");
+    
+}
+
+MiddlewareLayer::MiddlewareLayer(std::string PSUX_PORT, std::string PSUY_PORT, std::string PSUZ_PORT, std::string TMETER_PORT, std::string LINACT_PORT ){
+    
+    std::unique_ptr<DXKDP_PSU> PSU_X = std::make_unique<DXKDP_PSU>(PSUX_PORT, 0.01, 0.01);
+    std::unique_ptr<DXKDP_PSU> PSU_Y = std::make_unique<DXKDP_PSU>(PSUY_PORT, 0.01, 0.01);
+    std::unique_ptr<DXKDP_PSU> PSU_Z = std::make_unique<DXKDP_PSU>(PSUZ_PORT, 0.1, 0.1);
+
+    std::unique_ptr<Teslameter> T_Meter = std::make_unique<Teslameter>(TMETER_PORT);
+    std::unique_ptr<LinearActuator> LinAct = std::make_unique<LinearActuator>(LINACT_PORT);
+    
+}
+
+void MiddlewareLayer::TurnOnSupply(){
+    std::thread thread_x(&DXKDP_PSU::PoCtrl, *PSU_X, 0x01);
+    std::thread thread_y(&DXKDP_PSU::PoCtrl, *PSU_Y, 0x01);
+    std::thread thread_z(&DXKDP_PSU::PoCtrl, *PSU_Z, 0x01);
+    thread_x.join();
+    thread_y.join();
+    thread_z.join();
+}
+
+void MiddlewareLayer::TurnOffSupply(){
+    std::thread thread_x(&DXKDP_PSU::PoCtrl, *PSU_X, 0x00);
+    std::thread thread_y(&DXKDP_PSU::PoCtrl, *PSU_Y, 0x00);
+    std::thread thread_z(&DXKDP_PSU::PoCtrl, *PSU_Z, 0x00);
+    thread_x.join();
+    thread_y.join();
+    thread_z.join();
+}
+
+void MiddlewareLayer::initialSetup(){
+    std::thread thread_x(&DXKDP_PSU::WriteVoltage, *PSU_X, 60, 0x01);
+    std::thread thread_y(&DXKDP_PSU::WriteVoltage, *PSU_Y, 60, 0x01);
+    std::thread thread_z(&DXKDP_PSU::WriteVoltage, *PSU_Z, 60, 0x01);
+    thread_x.join();
+    thread_y.join();
+    thread_z.join();
+}
+
+void MiddlewareLayer::set3DVector(std::vector<float> I_X, std::vector<float> I_Y, std::vector<float> I_Z){
+    for(int i = 0; i < I_X.size(); i++){
+        std::thread thread_x(&DXKDP_PSU::WriteCurrent, *PSU_X, I_X[i], 0x01);
+        std::thread thread_y(&DXKDP_PSU::WriteCurrent, *PSU_Y, I_Y[i], 0x01);
+        std::thread thread_z(&DXKDP_PSU::WriteCurrent, *PSU_Z, I_Z[i], 0x01);
+        thread_x.join();
+        thread_y.join();
+        thread_z.join();
+    }
+}
