f43be1866a03aaff079c4b935065b617707885ea
Vittorio <vittorio.francescon@gmaill.com>
2022-07-24 19:02:22 +0100

    Added todo in precomps

--- src/precomputation.cpp
+++ src/precomputation.cpp
@@ -1,10 +1,28 @@
-#include "cl_tentacle/src/DataTypes.hpp"
+#include "precomputation.hpp"
 
 
 int main(int argc, char* argv[]){
     
     
+    /*
+    Precomputation checklist
+
+    Equation:
+        KQ = J^T * S * B
+
     
-    
+    //TODAY (24/07/22)
+    1. take care of calculating K
+    2. Take a Q input
+    3. Calculate J (and J^T) 
+
+    //MONDAY (25/07/22)
+    1. Figure out how to stack S 
+    2. Use all that to precalculate B at each timestep
+    */
+
+   
+
+
     return 0;
 }
