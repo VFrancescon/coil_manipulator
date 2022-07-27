5e6d5eec50b9b5cd130297a05b7a8fa0de07bd01
Vittorio Francescon <vittorio.francescon@gmail.com>
2022-07-27 15:00:50 +0100

    Added some error handling in setfield3d

--- src/set3DField.cpp
+++ src/set3DField.cpp
@@ -2,20 +2,31 @@
 
 
 int main(int argc, char *argv[]){
-    if(argc > 3){
+    float I_X, I_Y, I_Z;
+    if(argc == 4){
         std::cout << "ARSGS: " << argv[1] << " " << argv[2] << " " << argv[3] << "\n";
-    
-        float I_X = std::stof(argv[1]);
-        float I_Y = std::stof(argv[2]);        
-        float I_Z = std::stof(argv[3]);
-
-        MiddlewareLayer mid;
+        try{
+        I_X = std::stof(argv[1]);
+        I_Y = std::stof(argv[2]);        
+        I_Z = std::stof(argv[3]);
+        } catch( std::invalid_argument &e){
+            std::cout << "One of the provided arguments was invalid. Arguments:\n" << argv[1] << " " << argv[2] << " " << argv[3] << "\n";
+            return;
+        }
+        
+        bool psu_only_mode = true; 
+        MiddlewareLayer mid(psu_only_mode);
 
 
         mid.set3DField(I_X, I_Y, I_Z);
         std::cout << "Holding field as printed above. press enter to quit\n";
         std::cin.get();
+    } else{
+        std::cout << "Incorrect number of arguments provided\n.";
     }
+
+    std::cout << "./field3d <Bx> <By> <Bz>\n";
+    std::cout << "Each argument sets the respective field\n";
 
     return 0;
 }
