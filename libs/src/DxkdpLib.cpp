e88846a25a9b40c53db063e2b3edd5d30484b95b
Vittorio <vittorio.francescon@gmail.com>
2023-03-22 12:34:32 +0000

    Testing z2 polarity

--- libs/src/DxkdpLib.cpp
+++ libs/src/DxkdpLib.cpp
@@ -48,6 +48,7 @@ void DXKDP_PSU::PsuRead(output_message &msgOut)
 {
     // std::cout << "Started PSUread on " << this->PsuID << " \n";
     this->serialPort.ReadBinary(msgOut.output1);
+    // std::cout << "From psuread. size of returned values: " << msgOut.output1.size() << "\n";
     // std::cout << "Finished psuread on " << this->PsuID << " \n";
 }
 
@@ -304,19 +305,20 @@ void DXKDP_PSU::ReadVI(uint8_t addr)
 
 void DXKDP_PSU::setPolarity(uint8_t polarity, uint8_t addr)
 {
-    if (polarity > 0x01)
-    {
-        std::cout << "PsuID: " << this->PsuID << " ";
-        THROW_EXCEPT("Polarity setting called with polarity > 0x01. If you do require such value, please use GEN2 version of the function");
-    }
-    // std::cout << "-------------------SET POLARITY--------------------\n";
+    // if (polarity > 0x01)
+    // {
+    //     std::cout << "PsuID: " << this->PsuID << " ";
+    //     THROW_EXCEPT("Polarity setting called with polarity > 0x01. If you do require such value, please use GEN2 version of the function");
+    // }
     std::vector<uint8_t> input_vector = this->Encoder24(polarity, polarity, addr);
-    // for(auto i: input_vector) printf("%02X ", i);
-    // std::cout << "\n\n";
+    for(auto i: input_vector) printf("%02X ", i);
+    std::cout << "\n\n";
     this->PsuWrite(input_vector);
     usleep(50e3);
     output_message msgOut;
     this->PsuRead(msgOut);
+    std::cout << "Gen2 polarity. size of returned values: " << msgOut.output1.size() << "\n";
+
     if (msgOut.output1[0] != 0x06)
     {
         std::cout << "PsuID: " << this->PsuID << "\n";
@@ -333,20 +335,22 @@ void DXKDP_PSU::setPolarityGen2(uint8_t polarity, uint8_t addr)
 {
     // std::cout << "-------------------SET POLARITY--------------------\n";
     std::vector<uint8_t> input_vector = this->Encoder24(polarity, polarity, addr);
-    // for(auto i: input_vector) printf("%02X ", i);
-    // std::cout << "\n\n";
+    for(auto i: input_vector) printf("%02X ", i);
+    std::cout << "\n\n";
     this->PsuWrite(input_vector);
     usleep(50e3);
     output_message msgOut;
     this->PsuRead(msgOut);
-    if (msgOut.output1[0] != 0x06)
-    {
-        std::cout << "PsuID: " << this->PsuID << "\n";
-        THROW_EXCEPT("Polarity setting did not return 0x06. Aborting");
-    }
-    else
-    {
-        // std::cout << "-------------------SET POLARITY--------------------\n\n\n";
-        return;
-    }
+    std::cout << "Gen2 polarity. size of returned values: " << msgOut.output1.size() << "\n";
+
+    // if (msgOut.output1[0] != 0x06)
+    // {
+    //     std::cout << "PsuID: " << this->PsuID << "\n";
+    //     THROW_EXCEPT("Polarity setting did not return 0x06. Aborting");
+    // }
+    // else
+    // {
+    //     // std::cout << "-------------------SET POLARITY--------------------\n\n\n";
+    //     return;
+    // }
 }
--- src/psu_test.cpp
+++ src/psu_test.cpp
@@ -4,63 +4,87 @@
 
 int main(int argc, char* argv[]){
 
-    if(argc == 1){
-        DXKDP_PSU supply("/dev/ttyUSB2", 0.01, 0.01);
-        std::cout << "No input. Will enable Power out. Press enter to read VI.\n";
-        supply.WriteVI(10,5);
-        supply.PoCtrl(0x01);
-        std::cin.get();
+    // if(argc == 1){
+    //     DXKDP_PSU supply("/dev/ttyUSB2", 0.01, 0.01);
+    //     std::cout << "No input. Will enable Power out. Press enter to read VI.\n";
+    //     supply.WriteVI(10,5);
+    //     supply.PoCtrl(0x01);
+    //     std::cin.get();
         
-        supply.ReadVI();
-        std::cout << "\nPress enter to shut down\n";
-        std::cin.get();
-        supply.PoCtrl(0x00);
-    }
-    if(argc == 2){
-        std::string address = argv[1];
-        DXKDP_PSU supply(address, 0.01, 0.01);
-        supply.setPolarity(0x00);
-        supply.WriteVI(5,5);
-        supply.PoCtrl(0x01);
-        std::cout << "Press enter to flip polarity\n";
-        std::cin.get();
-        supply.setPolarity(0x01);
-        std::cin.get();
+    //     supply.ReadVI();
+    //     std::cout << "\nPress enter to shut down\n";
+    //     std::cin.get();
+    //     supply.PoCtrl(0x00);
+    // }
+    // if(argc == 2){
+    //     std::string address = argv[1];
+    //     DXKDP_PSU supply(address, 0.01, 0.01);
+    //     supply.setPolarity(0x00);
+    //     supply.WriteVI(5,5);
+    //     supply.PoCtrl(0x01);
+    //     std::cout << "Press enter to flip polarity\n";
+    //     std::cin.get();
+    //     supply.setPolarity(0x01);
+    //     std::cin.get();
 
-        supply.PoCtrl(0x00);
+    //     supply.PoCtrl(0x00);
 
-    }
+    // }
 
-    if(argc == 3){
-        DXKDP_PSU supply1("/dev/ttyUSB2", 0.01, 0.01);
-        DXKDP_PSU supply2("/dev/ttyUSB3", 0.01, 0.01);
+    // if(argc == 3){
+    //     DXKDP_PSU supply1("/dev/ttyUSB2", 0.01, 0.01);
+    //     DXKDP_PSU supply2("/dev/ttyUSB3", 0.01, 0.01);
         
-        supply1.PoCtrl(0x01);
-        supply2.PoCtrl(0x01);
+    //     supply1.PoCtrl(0x01);
+    //     supply2.PoCtrl(0x01);
 
-        //ALL OTHER CASES
-        supply1.setPolarity(0x01); //a
-        supply2.setPolarity(0x01); //b
+    //     //ALL OTHER CASES
+    //     supply1.setPolarity(0x01); //a
+    //     supply2.setPolarity(0x01); //b
 
-        //Y supplies case
-        // supply1.setPolarity(0x01); //b
-        // supply2.setPolarity(0x00); //a
+    //     //Y supplies case
+    //     // supply1.setPolarity(0x01); //b
+    //     // supply2.setPolarity(0x00); //a
         
         
         
-        int s12_v = std::stof(argv[1]);
-        int s12_i = std::stof(argv[2]);
+    //     int s12_v = std::stof(argv[1]);
+    //     int s12_i = std::stof(argv[2]);
 
-        supply1.WriteVI(s12_v, s12_i);
-        supply2.WriteVI(s12_v, s12_i);
+    //     supply1.WriteVI(s12_v, s12_i);
+    //     supply2.WriteVI(s12_v, s12_i);
         
-        std::cout << "Set VI. Press enter to exit\n";
-        std::cin.get();
-        supply1.WriteVI(0,0);
-        supply2.WriteVI(0,0);
-        supply1.PoCtrl(0x00);
-        supply2.PoCtrl(0x00);
-    }
+    //     std::cout << "Set VI. Press enter to exit\n";
+    //     std::cin.get();
+    //     supply1.WriteVI(0,0);
+    //     supply2.WriteVI(0,0);
+    //     supply1.PoCtrl(0x00);
+    //     supply2.PoCtrl(0x00);
+    // }
+
+    std::cout << "Testing polarity from z2 supply. Press enter to begin";
+    std::cin.get();
+    DXKDP_PSU psu1("/dev/ttyUSB2", 0.01, 0.01);
+    psu1.WriteVI(10,10);
+    psu1.PoCtrl(0x01);
+    std::cout << "Written 10V,10A and PO=1. Press enter to begin polarity tests";
+    std::cin.get();
+
+    std::cout << "0x00. Closed ";
+    psu1.setPolarity(0x00, 0x01);
+    std::cin.get();
+
+    std::cout << "0x01. Positive ";
+    psu1.setPolarity(0x01);
+    std::cin.get();
+
+    std::cout << "0x02. Negative ";
+    psu1.setPolarity(0x02);
+    std::cin.get();
+
+    std::cout << "0x03. LDI ";
+    psu1.setPolarity(0x03);
+    std::cin.get();
 
     return 0;
 }
